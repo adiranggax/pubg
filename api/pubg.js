1f6b9fa0b5056cef7ea05aa82243c0a703114ea4
Adirangga <127793665+adiranggax@users.noreply.github.com>
2026-03-13 12:40:01 +0800

    Update pubg.js

--- api/pubg.js
+++ api/pubg.js
@@ -1,18 +1,17 @@
 export default async function handler(req, res) {
 
-const id = req.query.id
+const { id } = req.query
 
 if(!id){
  return res.json({
   status:false,
-  message:"ID kosong"
+  message:"Masukkan ID"
  })
 }
 
 try{
 
-const payload = `userid:${id}`
-const encoded = Buffer.from(payload).toString("base64")
+const encoded = Buffer.from(`userid:${id}`).toString("base64")
 
 const url =
 `https://api.tokogame.com/core/v1/orders/validate-order?productId=63338bc5d16e41172ceb0466&encryptedAnswers=${encodeURIComponent(encoded)}`
@@ -20,18 +19,29 @@ const url =
 const r = await fetch(url,{
  method:"GET",
  headers:{
-  "accept":"application/json",
+  "accept":"application/json, text/plain, */*",
   "x-region":"ID",
   "x-language":"ID",
   "x-currency":"IDR",
-  "referer":"https://www.tokogame.com/"
+  "x-request-id":"test123",
+  "referer":"https://www.tokogame.com/",
+  "origin":"https://www.tokogame.com",
+  "user-agent":"Mozilla/5.0"
  }
 })
 
 const data = await r.json()
 
+if(data.code === "SUCCESS"){
+ return res.json({
+  status:true,
+  nickname:data.data.username
+ })
+}
+
 res.json({
- status:true,
+ status:false,
+ message:"ID tidak ditemukan",
  raw:data
 })
 
