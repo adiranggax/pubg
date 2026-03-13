8f20051e2ca4bb529b5ad3420bee4edb3370a2aa
Adirangga <127793665+adiranggax@users.noreply.github.com>
2026-03-13 11:38:54 +0800

    Update pubg.js

--- api/pubg.js
+++ api/pubg.js
@@ -3,9 +3,9 @@ export default async function handler(req, res) {
 const { id } = req.query
 
 if(!id){
- return res.status(400).json({
+ return res.json({
   status:false,
-  message:"Masukkan ID"
+  message:"Masukkan id"
  })
 }
 
@@ -16,24 +16,34 @@ const encoded = Buffer.from(`userid:${id}`).toString("base64")
 const url =
 `https://api.tokogame.com/core/v1/orders/validate-order?productId=63338bc5d16e41172ceb0466&encryptedAnswers=${encoded}`
 
-const response = await fetch(url,{
+const r = await fetch(url,{
  headers:{
+  "accept":"application/json",
   "x-region":"ID",
   "x-language":"ID",
-  "x-currency":"IDR"
+  "x-currency":"IDR",
+  "referer":"https://www.tokogame.com/",
+  "user-agent":"Mozilla/5.0"
  }
 })
 
-const data = await response.json()
+const data = await r.json()
+
+if(data.code !== "SUCCESS"){
+ return res.json({
+  status:false,
+  message:"ID tidak ditemukan"
+ })
+}
 
 res.json({
  status:true,
- nickname:data?.data?.username || null
+ nickname:data.data.username
 })
 
 }catch(e){
 
-res.status(500).json({
+res.json({
  status:false,
  error:e.message
 })
